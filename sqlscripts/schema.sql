ad33cfe299d819fc7ad9e58c54525a7c9e972517
Krishna P. Srinivasmurthy <krishna.p.srinivasmurthy@boeing.com>
2015-07-13 19:32:49 -0700

    created all the tables and their JPA's

--- sqlscripts/schema.sql
+++ sqlscripts/schema.sql
@@ -14,4 +14,68 @@ create table book (
     isbn character varying(45),
     category_id integer REFERENCES category (id),
     CONSTRAINT book_pkey PRIMARY KEY (id)
-)
+);
+
+create table address (
+    id serial,
+    street character varying(45),
+    houseNumber character varying(45),
+    boxNumber character varying(45),
+	city character varying(45),
+    postalCode character varying(45),
+    country character varying(45),
+    CONSTRAINT address_pkey PRIMARY KEY (id)
+);
+
+create table account (
+	id serial,
+	firstName character varying(45),
+    lastName character varying(45),
+	dateOfBirth date,
+    address_id integer REFERENCES address (id),
+    emailAddress character varying(45),
+	username character varying(45),
+	password character varying(45),
+    CONSTRAINT account_pkey PRIMARY KEY (id)
+);
+
+create table role (
+	id serial,
+	role character varying(45),
+    CONSTRAINT role_pkey PRIMARY KEY (id)
+);
+
+create table account_role (
+	id serial,
+	account_id integer REFERENCES account (id),
+	role_id integer REFERENCES role (id),
+    CONSTRAINT account_role_pkey PRIMARY KEY (id)
+);
+
+create table permission (
+	id serial,
+	permission character varying(45),
+    CONSTRAINT permission_pkey PRIMARY KEY (id)
+);
+
+create table role_permission (
+	id serial,
+	role_id integer REFERENCES role (id),
+	permission_id integer REFERENCES permission (id),
+    CONSTRAINT role_permission_pkey PRIMARY KEY (id)
+);
+
+create table order_details (
+	id serial,
+	book_id integer REFERENCES book (id),
+	quantity decimal,
+    CONSTRAINT order_details PRIMARY KEY (id)
+);
+
+create table orders (
+	id serial,
+	shipping_address_id integer REFERENCES address (id),
+	billing_address_id integer REFERENCES address (id),
+	account_id integer REFERENCES  account (id),	
+    CONSTRAINT order_details PRIMARY KEY (id)
+);
